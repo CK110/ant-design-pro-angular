fcec67401b6b59a647a0aa85943fcd0d8dbc5a33
CK <chenkai4dev@163.com>
2020-02-03 14:12:31 +0800

    feat(pro): add acl demo

--- src/app/pages/other/permission-list/permission-list.component.ts
+++ src/app/pages/other/permission-list/permission-list.component.ts
@@ -1,13 +1,22 @@
 import {Component, OnInit} from '@angular/core';
+import {ACLService} from '@delon/acl';
 
 @Component({
   selector: 'app-permission-list',
   templateUrl: 'permission-list.component.html'
 })
 export class PermissionListComponent implements OnInit {
-  constructor() {
+  constructor(private aclService: ACLService) {
   }
 
   ngOnInit() {
+  }
+
+  addRight() {
+    this.aclService.setAbility([1, 2])
+  }
+
+  clearRight(){
+    this.aclService.setAbility([]);
   }
 }
